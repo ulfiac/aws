aed202b944340b7e33ce76245bf338359eb63a0d
ulfiac <221777317+ulfiac@users.noreply.github.com>
2025-07-27 20:06:46 -0400

    add s3 backend

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,3 +1,10 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_kms_key" "aws_s3_key" {
+  key_id = "alias/aws/s3"
+}
+
 locals {
-  iam_role_name_admin = "ulfiac-oidc-gha-admin"
+  bucket_name     = "ulfiac-terraform-state"
+  role_name_admin = "ulfiac-oidc-gha-admin"
 }
